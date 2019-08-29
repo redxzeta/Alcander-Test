269372ecc5eff80c644c3c1a554c8e8582dfb37c
redxzeta <xzetanzero@gmail.com>
2019-08-28 21:29:50 -0700

    working

--- django_projects/locallibrary/catalog/views.py
+++ django_projects/locallibrary/catalog/views.py
@@ -1,5 +1,7 @@
+from django.views import generic
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
+
 
 def index(request):
     """View function for home page of site."""
@@ -7,22 +9,44 @@ def index(request):
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-    p = Book.objects.all()[:1].get()
     # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    
-    # The 'all()' is implied by default.    
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a').count()
+
+    # The 'all()' is implied by default.
     num_authors = Author.objects.count()
-    first = Author.objects.all()[:1].get()
+
+    # Number of visits to this view, as counted in the session variable.
+    """     num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1 """
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'p': p,
-        'first': first,
+        # 'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    """Generic class-based list view for a list of authors."""
+    model = Author
+    paginate_by = 10
+
+
+class AuthorDetailView(generic.DetailView):
+    """Generic class-based detail view for an author."""
+    model = Author
